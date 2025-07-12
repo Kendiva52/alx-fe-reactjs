ca7c18aba124952ec57d2c122287fd046cafcecc
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-07-12 18:52:24 +0100

    Update MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 function MainContent(){
     return(
-        <div>
-            <main>
-                <p>i love to visit New York, Paris, and Tokyo.</p>
+           <main>
+              <p>I love to visit New York, Paris, and Tokyo.</p>
             </main>
         </div>
     );
