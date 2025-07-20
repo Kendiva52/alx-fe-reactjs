335aa206aad1c37400d33265a3ca33d83cb7ac52
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-07-20 11:09:23 +0100

    Update MainContent.jsx

--- alx-react-app-new/ src/components/MainContent.jsx
+++ alx-react-app-new/ src/components/MainContent.jsx
@@ -3,12 +3,7 @@ import React from "react";
 function MainContent(){
     return(
         
-            <main style={{
-                padding: '15px',
-                fontSize: '18px',
-                lineHeight: '1.6',
-                color: '#333',
-            }}>
+            <main style={{ padding: '15px', fontSize: '18px', lineHeight: '1.6',color: '#333'}}>
                 <p>i love to visit New York, Paris, and Tokyo.</p>
             </main>
         
