7a6952398935dabc87c9dc520780151d58a25bb5
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-07-20 10:40:35 +0100

    Add files via upload

---
+++ alx-react-app-new/ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <header style={{
+                backgroundColor:'navy',
+                color: 'white',
+                textAlign: 'center',
+            }}>
+                <hi>My Favorite Cities</hi>
+            </header>
+        </div>
+    );
+}
+export default Header;
---
+++ alx-react-app-new/ src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MainContent(){
+    return(
+        
+            <main style={{
+                padding: '15px',
+                fontSize: '18px',
+                lineHeight: '1.6',
+                color: '#333',
+            }}>
+                <p>i love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        
+    );
+}
+export default MainContent;
+
