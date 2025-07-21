ecf6f104176a82fa8986fd41683a70ea8aaabde5
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-07-21 10:58:13 +0100

    Add files via upload

---
+++ alx-react-app-props/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <header>
+                <hi>My Favorite Cities</hi>
+            </header>
+        </div>
+    );
+}
+export default Header;
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainContent(){
+    return(
+        
+            <main>
+                <p>i love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        
+    );
+}
+export default MainContent;
+
