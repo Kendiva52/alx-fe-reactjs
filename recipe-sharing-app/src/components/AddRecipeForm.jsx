0bee0a89f3ae94472ff34c186ea0fa3a69081e96
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-07-29 09:46:52 +0100

    Update AddRecipeForm.jsx

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useRecipeStore } from '../store/recipeStore';
 
-const AddRecipeForm = () => {
+export default funtion AddRecipeForm () {
   const addRecipe = useRecipeStore((state) => state.addRecipe);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
@@ -16,24 +16,12 @@ const AddRecipeForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Add a Recipe</h2>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Title"
-        
-      />
-      <textarea
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder="Description"
-        required
-      />
-      
+      <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" required />
+      <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" required />
       <button type="submit">Add Recipe</button>
     </form>
   );
-};
+}
+    
 
 export default AddRecipeForm;
