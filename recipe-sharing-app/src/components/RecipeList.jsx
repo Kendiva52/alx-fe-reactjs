47ff276f2aa4ed79182570911d45753c598a7e11
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-07-29 09:57:18 +0100

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,22 +1,34 @@
-// src/components/RecipeList.jsx
 import { useRecipeStore } from '../store/recipeStore';
+import { Link } from 'react-router-dom';
+import FavoriteToggle from './FavoriteToggle';
+
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
-  return (
+ return (
     <div>
-      <h2>Recipes</h2>
-      {recipes.length === 0 ? (
-        <p>No recipes yet.</p>
+      {filteredRecipes.length === 0 ? (
+        <p>No recipes found.</p>
       ) : (
-        recipes.map((recipe) => (
+        filteredRecipes.map((recipe) => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
+            <h3>
+              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+            </h3>
+            <p>{recipe.description.slice(0, 50)}...</p>
+            <FavoriteToggle recipeId={recipe.id} />
           </div>
         ))
       )}
+      const isFavorite = favorites.includes(recipe.id);
+      <button
+        onClick={() => {
+          isFavorite ? removeFavorite(recipe.id) : addFavorite(recipe.id);
+        }}
+      >
+        {isFavorite ? "★ Remove from Favorites" : "☆ Add to Favorites"}
+      </button>
     </div>
   );
 };
