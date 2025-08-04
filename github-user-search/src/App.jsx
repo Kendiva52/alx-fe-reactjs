b65bd0063f262d71593e0d97ae28da0d5701a235
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-08-04 12:13:42 +0100

    Add files via upload

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import Search from './components/Search';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>GitHub User Search</h1>
+      <Search />
+      
+    </div>
+  );
+};
+
+export default App;
