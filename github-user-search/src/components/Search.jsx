e3db161a0c822c1374f3945b54f5e626d141ed05
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-08-05 09:36:09 +0100

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { searchUsers } from '../services/githubService';
+import { fetchUserData } from '../services/githubService';
 
 const Search = () => {
   const [username, setUsername] = useState('');
@@ -16,7 +16,7 @@ const Search = () => {
     setUsers([]);
 
     try {
-      const data = await searchUsers(username, location, minRepos);
+      const data = await fetchUserData(username, location, minRepos);
       setUsers(data);
     } catch (err) {
       setError('Failed to fetch users. Try again.');
@@ -86,3 +86,4 @@ const Search = () => {
 };
 
 export default Search;
+
