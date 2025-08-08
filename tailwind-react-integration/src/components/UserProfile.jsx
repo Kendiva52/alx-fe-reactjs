4247bf96f06cb40038bf1784a6f3aeed51fb2705
Kendiva52 <111061141+Kendiva52@users.noreply.github.com>
2025-08-08 10:00:11 +0100

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
         <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center hover:shadow-xl">
-            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out mx-auto" />
+            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out mx-auto" />
             <h1 className="sm:text-lg md:text-xl text-blue-800 hover:text-blue-500 my-4">John Doe</h1>
             <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Lovers to write code and explore new technologies.</p>
         </div> 
